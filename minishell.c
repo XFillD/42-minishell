37beefd0f7d92e76c4c6574aeb7effbd977727aa
XFillD <93709236+XFillD@users.noreply.github.com>
2024-06-30 12:31:29 +0200

    cd test (#7)
    * cd test
    
    * cd works
    
    * fixed free
    
    ---------
    
    Co-authored-by: Yana Alechina <yalechin@c3r7s2.42prague.com>

--- minishell.c
+++ minishell.c
@@ -6,11 +6,18 @@
 /*   By: yalechin <yalechin@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 10:31:55 by yalechin          #+#    #+#             */
-/*   Updated: 2024/06/22 13:53:52 by yalechin         ###   ########.fr       */
+/*   Updated: 2024/06/23 20:21:05 by yalechin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+typedef struct s_envp
+{
+	char			*var_name;
+	char			*var_value;
+	struct s_envp	*next;
+}					t_envp;
 
 typedef struct s_token
 {
@@ -27,10 +34,29 @@ typedef struct s_program
 	char			*input;
 	char			*split_line;
 	t_token			*first;
-	char			**envp;
-	char			**paths;
+	//char			**envp;
+	t_envp *envp; 
+	//char			**paths;
 
 }					t_program;
+
+
+
+bool	streq(char *str1, char *str2)
+{
+	size_t	i;
+
+	if ((str1 && !str2) || (!str1 && str2))
+		return (false);
+	i = 0;
+	while (str1[i] || str2[i])
+	{
+		if (str1[i] != str2[i])
+			return (false);
+		i += 1;
+	}
+	return (true);
+}
 
 // compare strings
 int	ft_strcmp(char *s1, char *s2)
@@ -84,10 +110,10 @@ int	ft_check_quotes(t_program *program)
 	}
 	if (!q1 && !q2)
 	{
-		printf("ALL GOOD - QUOTES MATCH\n");
+		//printf("ALL GOOD - QUOTES MATCH\n");
 		return (0);
 	}
-	printf("NOT GOOD - QUOTES DO NOT MATCH\n");
+	//printf("NOT GOOD - QUOTES DO NOT MATCH\n");
 	return (1);
 }
 
@@ -402,44 +428,177 @@ int	ft_check_tokens(t_program *program)
 	return (1);
 }
 
-void	ft_free_program(t_program *program)
-{
-	/*int	x;
-	x = 0;
-	while (program->first)
-	{
-		free(program->first);
-		x++;
-	}*/
-	free(program->input);
-	free(program);
+
+
+
+void clear_token_list(t_token *head) {
+    t_token *current = head;
+    t_token *next;
+
+    // Clear the token linked list
+    while (current != NULL) {
+        next = current->next;
+        free(current->token_str);  
+        free(current);
+        current = next;
+    }
+
 }
 
-// stores the list of envp in the program structure
-char	**ft_store_envp(char **envp)
-{
-	int		x;
-	char	**envp_arr;
+void ft_free_program(t_program *program) {
+    //clear_token_list(program);
+	t_envp *current = program->envp;
+    t_envp *next;
 
-	x = 0;
-	while (envp[x])
-	{
-		x++;
-	}
-	envp_arr = (char **)malloc(sizeof(char *) * x + 1);
-	if (!envp_arr)
-		return (NULL);
-	x = 0;
-	while (envp[x])
-	{
-		envp_arr[x] = ft_strdup(envp[x]);
-		x++;
-	}
-	// free smth?
-	return (envp_arr);
+    // Clear the token linked list
+    while (current != NULL) {
+        next = current->next;
+        free(current->var_name);  
+        free(current);
+        current = next;
+    }
+
+    free(program->input);
+    free(program->split_line); 
+
+    free(program);
 }
 
-// find PATH in envp
+
+
+t_envp	*v_lstlast(t_envp *node)
+{
+	while (node)
+	{
+		if (!node->next)
+			break ;
+		node = node->next;
+	}
+	return (node);
+}
+
+void	v_lstadd_back(t_envp **head, t_envp *new)
+{
+	t_envp	*temp;
+
+	if (head)
+	{
+		if (!*head)
+			*head = new;
+		else
+		{
+			temp = v_lstlast(*(head));
+			temp->next = new;
+		}
+	}
+}
+
+
+int	unset_var(char *var_name, t_envp **head)
+{
+	t_envp	*temp;
+	t_envp	*next_node;
+
+	temp = *head;
+	if (ft_strrchr(var_name, '='))
+	{
+		//invalid_identifer(var_name);
+		return (EXIT_FAILURE);
+	}
+	while (temp && temp->next != NULL)
+	{
+		if (streq(var_name, temp->next->var_name))
+		{
+			next_node = temp->next->next;
+			free(temp->next->var_name);
+			free(temp->next->var_value);
+			free(temp->next);
+			temp->next = next_node;
+			break ;
+		}
+		temp = temp->next;
+	}
+	return (EXIT_SUCCESS);
+}
+
+char	**split_envp(char *env)
+{
+	char	**splitted;
+	size_t	indexof_equal;
+
+	splitted = malloc(3 * sizeof(char *));
+	indexof_equal = ft_strchr(env, '=') - env;
+	splitted[0] = ft_substr(env, 0, indexof_equal);
+	splitted[1] = ft_substr(env, indexof_equal + 1,
+			ft_strlen(&env[indexof_equal]));
+	splitted[2] = NULL;
+	return (splitted);
+}
+
+t_envp	*v_new_node(char *var_name, char *var_value)
+{
+	t_envp	*new_node;
+
+	new_node = malloc(sizeof(t_envp));
+	new_node->var_name = var_name;
+	new_node->var_value = var_value;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+int	save_user_vars(char *statement, t_envp **head)
+{
+	char	**line;
+
+	line = split_envp(statement);
+	//if (get_exported_state(line[0], head))
+		//to_export = true;
+	unset_var(line[0], head);
+	v_lstadd_back(head, v_new_node(line[0], line[1]));
+	free(line);
+	return (EXIT_SUCCESS);
+}
+
+
+static void	init_oldpwd(t_envp **head)
+{
+	char	*temp;
+
+	unset_var("OLDPWD", head);
+	temp = ft_strjoin("OLDPWD=", getenv("HOME"));
+	save_user_vars(temp, head);
+	free(temp);
+}
+
+
+
+
+t_envp	*ft_init_envp_list(char **envp)
+{
+	t_envp *head; 
+	t_envp *temp;
+
+	char **line; 
+	size_t	i;
+
+	line = split_envp(envp[0]);
+	head = v_new_node(line[0], line[1]);
+	free(line);
+	i = 1;
+	temp = head;
+	while (envp[i])
+	{
+		line = split_envp(envp[i]);
+		temp->next = v_new_node(line[0], line[1]);
+		free(line);
+		temp = temp->next;
+		i += 1;
+	}
+	init_oldpwd(&head);
+	return (head);
+}
+
+/*// find PATH in envp
 char	*ft_find_path(t_program *program)
 {
 	int	x;
@@ -482,11 +641,176 @@ void	ft_create_paths(t_program *program)
 		x++;
 	}
 	// testing print - delete later
-	x = 0;
-	while (program->paths[x])
+	// x = 0;
+	// while (program->paths[x])
+	// {
+	// 	printf("%s\n", program->paths[x]);
+	// 	x++;
+	// }
+}*/
+
+static int	print_perror_msg(char *path)
+{
+	char	*error_msg;	
+
+	error_msg = ft_strjoin("minishell: cd: ", path);
+	perror(error_msg);
+	free(error_msg);
+	return (EXIT_FAILURE);
+}
+
+static void	update_pwd(t_program *program)
+{
+	char	cwd[4096];
+	char	*updated_var;
+
+	getcwd(cwd, 4096);
+	updated_var = ft_strjoin("PWD=", cwd);
+	save_user_vars(updated_var, &program->envp);
+	free(updated_var);
+}
+
+static void	update_oldpwd(char *temp, t_program *program)
+{
+	char	*oldpwd;
+
+	oldpwd = ft_strjoin("OLDPWD=", temp);
+	save_user_vars(oldpwd, &program->envp);
+	free(oldpwd);
+}
+
+char	*get_fromvlst(char *var_name, t_envp **head)
+{
+	t_envp	*temp;
+
+	temp = *head;
+	while (temp != NULL)
 	{
-		printf("%s\n", program->paths[x]);
-		x++;
+		if (streq(var_name, temp->var_name))
+			return (temp->var_value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
+
+static int	cd_oldpwd(char *temp, t_program *program)
+{
+	char	*oldpwd;
+
+	oldpwd = get_fromvlst("OLDPWD", &program->envp);
+	if (!oldpwd)
+	{	
+		//ft_putendl_fd(OLDPWD_NOT_SET, STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	if (chdir(oldpwd) == 0)
+	{
+		ft_putendl_fd(oldpwd, STDOUT_FILENO);
+		update_oldpwd(&temp[0], program);
+		update_pwd(program);
+		return (EXIT_SUCCESS);
+	}
+	return (print_perror_msg(oldpwd));
+}
+int	cmd_pwd(void)
+{
+	char	*cwd;
+
+	cwd = NULL;
+	cwd = getcwd(cwd, 0);
+	if (cwd == NULL)
+	{
+		perror("minishell:");
+		return (EXIT_FAILURE);
+	}
+	ft_putendl_fd(cwd, STDOUT_FILENO);
+	free(cwd);
+	return (EXIT_SUCCESS);
+}
+
+int	cmd_cd(char *path, t_program *program)
+{
+	char	temp[4096];
+
+	getcwd(temp, 4096);
+	if (path == NULL || streq(path, "~"))
+	{
+		update_oldpwd(&temp[0], program);
+		//printf("cmd tady\n");
+		chdir(getenv("HOME"));
+		update_pwd(program);
+
+		return (EXIT_SUCCESS);
+	}
+	if (streq(path, "-"))
+		return (cd_oldpwd(&temp[0], program));
+	if (chdir(path) == 0)
+	{
+		update_oldpwd(&temp[0], program);
+		update_pwd(program);
+		//printf("cmd tadyyyyy\n");
+		cmd_pwd();
+		return (EXIT_SUCCESS);
+	}
+	return (print_perror_msg(path));
+}
+
+static int	call_cmd_cd(char *dest, t_program *program)
+{
+	return (cmd_cd(dest, program));
+}
+
+void	ft_putendl_fd(char *str, int fd)
+{
+	ft_putstr_fd(str, fd);
+	write(fd, "\n", 1);
+}
+
+
+
+void	ft_execute(t_program *program)
+{
+	t_token	*temp;
+	int		x;
+
+	temp = program->first;
+	x = 0;
+	while (temp != NULL)
+	{
+		if (temp->token_type == CMD)
+		{
+			if (ft_strcmp(temp->token_str, "cd") == 0)
+			{
+				//printf("CD COMMAND FOUND\n");
+				call_cmd_cd(temp->next->token_str, program);
+			}
+			else if (ft_strcmp(temp->token_str, "echo") == 0)
+			{
+				printf("ECHO COMMAND FOUND\n");
+			}
+			else if (ft_strcmp(temp->token_str, "pwd") == 0)
+			{
+				//printf("PWD COMMAND FOUND\n");
+				cmd_pwd();
+			}
+			else if (ft_strcmp(temp->token_str, "export") == 0)
+			{
+				printf("EXPORT COMMAND FOUND\n");
+			}
+			else if (ft_strcmp(temp->token_str, "unset") == 0)
+			{
+				printf("UNSET COMMAND FOUND\n");
+			}
+			else if (ft_strcmp(temp->token_str, "env") == 0)
+			{
+				printf("ENV COMMAND FOUND\n");
+			}
+			else if (ft_strcmp(temp->token_str, "exit") == 0)
+			{
+				printf("EXIT COMMAND FOUND\n");
+			}
+		}
+		temp = temp->next;
 	}
 }
 
@@ -494,6 +818,7 @@ int	main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	(void)av;
+	(void)envp; 
 	t_program *program;
 
 	if (ac != 1 || av[1])
@@ -503,22 +828,30 @@ int	main(int ac, char **av, char **envp)
 	}
 
 	program = malloc(sizeof(t_program));
+	if(!program)
+		exit(1);
 	program->input = NULL;
-	program->envp = NULL;
+	program->split_line = NULL; 
+	program->first = NULL; 
+	program->envp = NULL; 
+	program->envp = ft_init_envp_list(envp);
 
-	program->envp = ft_store_envp(envp);
-	int x = 0;
-	while (program->envp[x])
-	{
-		printf("%s\n", program->envp[x]);
-		x++;
-	}
-	char *path = ft_find_path(program);
-	printf("PATH FOUND: %s\n", path);
 
-	ft_create_paths(program);
+	//program->envp = NULL;
 
-	/*while (1)
+	//program->envp = ft_store_envp(envp);
+	//int x = 0;
+	//while (program->envp[x])
+	//{
+	//	printf("%s\n", program->envp[x]);
+	//	x++;
+	//}
+	//char *path = ft_find_path(program);
+	// printf("PATH FOUND: %s\n", path);
+
+	//ft_create_paths(program);
+
+	while (1)
 	{
 		program->input = readline("minishell> ");
 		if (!program->input)
@@ -528,13 +861,13 @@ int	main(int ac, char **av, char **envp)
 		add_history(program->input);
 		ft_check_quotes(program);
 		program->split_line = ft_prepare_line(program);
-		printf("Final line: %s\n", program->split_line);
+		// printf("Final line: %s\n", program->split_line);
 		program->first = ft_tokenization(program);
 
-		t_token *temp = program->first;
+		//t_token *temp = program->first;
 		ft_token_type(program);
 
-		while (temp != NULL)
+		/*while (temp != NULL)
 		{
 			printf("TOKEN is: [%s] token type is [%d]\n", temp->token_str,
 				temp->token_type);
@@ -542,11 +875,13 @@ int	main(int ac, char **av, char **envp)
 		}
 
 		if (ft_check_tokens(program))
-			printf("ALL TOKEN GOOD!\n");
+			printf("ALL TOKEN GOOD!\n");*/
 
-		// ft_execute(program);
-
-		ft_free_program(program);
-	}*/
+		ft_execute(program);
+		clear_token_list(program->first);
+		program->first = NULL;
+		
+	}
+	ft_free_program(program);
 	return (0);
 }
